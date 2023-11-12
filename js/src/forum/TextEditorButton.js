b48e24f38d116e518dbe47676b48c4ae952fe666
Litalino <99712477+Litalino@users.noreply.github.com>
2023-11-12 15:22:15 +0700

    Update TextEditorButton.js
    Update TAB - DETAILS

--- js/src/forum/TextEditorButton.js
+++ js/src/forum/TextEditorButton.js
@@ -22,6 +22,8 @@ import LogInModal from "flarum/forum/components/LogInModal";
 import PostStream from "flarum/forum/components/PostStream";
 import Separator from "flarum/components/Separator";
 
+import ComposerPostPreview from "flarum/forum/components/ComposerPostPreview";
+
 export default class TextEditorButton extends Component {
   view() {
     return Dropdown.component(
@@ -550,6 +552,45 @@ app.initializers.add("litalino/more-bbcode", () => {
       </TextEditorButton>
     );
   });*/
+
+  //bbcode-tabs
+  let id = 0;
+
+
+  const createTabs = function () {
+    const containers = this.$(".tabs");
+
+    containers.each((i, container) => {
+      const $container = $(container);
+
+      if ($container.find('input[type="radio"][name]').length) return;
+
+      const $inputs = $container.find('> .tab > input[type="radio"]');
+
+      if (!$inputs.length) return;
+
+      const $items = $container.find(".tab");
+      const num = id++;
+
+      $inputs.attr("name", `tab-group-${num}`);
+
+      if (!$inputs.is("[checked]"))
+        $inputs[0].setAttribute("checked", "checked");
+
+      $items.each((i, item) => {
+        const $item = $(item);
+        const id = `tab-${num}-${++i}`;
+
+        $item.find('input[type="radio"]').attr("id", id);
+        $item.find("label").attr("for", id);
+      });
+    });
+  };
+
+  extend(CommentPost.prototype, ["oncreate", "onupdate"], createTabs);
+  extend(ComposerPostPreview.prototype, "oncreate", function () {
+    extend(this.attrs, "surround", () => createTabs.call(this));
+  });
 
 });
 
